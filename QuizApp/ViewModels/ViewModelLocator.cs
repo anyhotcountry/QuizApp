fad159ef9e0afb7d9c23b1ee71aed1ac0b1c1e30
Christian Mare <chris@mare.org.uk>
2015-12-23 14:02:03 +0000

    Added ViewModelLocator

---
+++ QuizApp/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿namespace QuizApp.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private QuizPageViewModel quizPageViewModel;
+
+        public QuizPageViewModel QuizPageViewModel
+        {
+            get
+            {
+                return quizPageViewModel = quizPageViewModel ?? new QuizPageViewModel();
+            }
+        }
+    }
+}
